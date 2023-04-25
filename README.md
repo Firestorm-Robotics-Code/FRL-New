7041c2be9785669ac562052de00b4dffa581adfe
WPILib Sucks <lukewhite32@icloud.com>
2023-04-25 16:25:11 -0400

    add use.py

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# FRL-New
-A reprogrammed version of FRL (firestorm robotics library)
+# FRL New
+A reprogrammed version of FRL (firestorm robotics library).
+# Not complete!
+It is still under development.
+# How to use
+Download use.py in the same directory as your robot project and add this to the end of build.gradle:
+```
+task loadFirestormRoboticsLibrary(type: Exec) {
+    commandLine 'python', 'use.py'
+}
+
+build.dependsOn loadFirestormRoboticsLibrary
+```
+You may need to build it once just so it can install (it's a bug in Gradle).
+Requires internet connection to install.
---
+++ use.py
@@ -0,0 +1,22 @@
+import os
+
+try:
+    print("Downloading FRL now. Internet connection is required. Nothing will install if there is no internet.")
+    if not os.path.exists("src/main/include"):
+        print("Error! Could not find path 'src/main/include'.")
+    else:
+        try:
+            if not os.path.exists("src/main/include/frl/base"):
+                os.system("mkdir src/main/include/frl > /dev/null 2>&1")
+        except:
+            print("Error adding a new directory! Make sure that the path 'src/main/include' exists. Installing FRL will not work unless it does.")
+    
+        if os.path.exists("src/main/include/frl"):
+            try:
+                os.system("rm -rf src/main/include/frl")
+                os.system("git clone https://github.com/Firestorm-Robotics-Code/FRL-New src/main/include/frl > /dev/null 2>&1")
+            except:
+                print("Unable to install FRL! Internet connection required!")
+
+except:
+    print("Error while installing!")
